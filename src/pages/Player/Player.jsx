ef63843e39555ce3ced9c57c1c404df35057da71
HH <shweehow@gmail.com>
2025-08-13 19:11:48 +0800

    Bug fixes for player file

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import { usesNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
 
